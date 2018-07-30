0eb729bbca7246cdbd61f7f2b2e4041963b317e5
wangning <batterspring@163.com>
2018-07-30 19:39:36 +0800

    init

---
+++ src/main/java/com/batterspring/App.java
@@ -0,0 +1,13 @@
+package com.batterspring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
